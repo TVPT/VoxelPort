87219bdee27c7b49e1ab7ba73508a8ca9e1d005a
przerwap <przerwap@gmail.com>
2012-04-16 11:08:31 -0700

    2.047 - Updated HitBlox.java to new version that accounts for new world height

--- src/main/java/com/thevoxelbox/voxelport/HitBlox.java
+++ src/main/java/com/thevoxelbox/voxelport/HitBlox.java
@@ -1,30 +1,19 @@
 package com.thevoxelbox.voxelport;
 
-import org.bukkit.block.Block;
 import org.bukkit.Location;
-import org.bukkit.entity.Player;
 import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
 
 /**
- * HitBlox.java - Class for getting blocks along line of sight
- * 
- * NOTES: This class is designed to handle the annoying parts of the seemingly
- * simple task of getting the coordinates of the block a player is currently
- * aimed at. This class abstracts the simpler tasks of finding the current
- * target block and the adjacent unoccupied block to their own methods, but it
- * also provides a public getNextBlock method for processing the entire
- * line-of-sight from the player for more specialized tasks. This method can be
- * used exactly as it is in getTargetBlock, for instance.
- * 
- * 
- * @author Ho0ber
+ * @author Voxel
  */
 public class HitBlox {
 
     private Location player_loc;
     private double rot_x, rot_y, view_height, rot_xsin, rot_xcos, rot_ysin, rot_ycos;
     private double length, h_length, step;
-    private int range;
+    private double range;
     private double player_x, player_y, player_z;
     private double x_offset, y_offset, z_offset;
     private int last_x, last_y, last_z;
@@ -38,8 +27,14 @@ public class HitBlox {
      */
     public HitBlox(Player in_player, World world) {
         w = world;
-        init(in_player.getLocation(), 200, 0.2, 1.65); // Reasonable default
+        init(in_player.getLocation(), 250, 0.2, 1.65); // Reasonable default
         // values
+    }
+
+    public HitBlox(Player p, World world, double maxRange) {
+        w = world;
+        init(p.getLocation(), maxRange, 0.2, 1.65);
+        fromOffworld();
     }
 
     /**
@@ -70,10 +65,11 @@ public class HitBlox {
      * @param in_step
      */
     public HitBlox(Location in_location, int in_range, double in_step) {
+        w = in_location.getWorld();
         init(in_location, in_range, in_step, 0);
     }
 
-    private void init(Location in_location, int in_range, double in_step, double in_view_height) {
+    private void init(Location in_location, double in_range, double in_step, double in_view_height) {
         player_loc = in_location;
         view_height = in_view_height;
         player_x = player_loc.getX();
@@ -103,6 +99,7 @@ public class HitBlox {
      * @return Block
      */
     public Block getTargetBlock() {
+        fromOffworld();
         while ((getNextBlock() != null) && (getCurBlock().getTypeId() == 0));
         return getCurBlock();
     }
@@ -145,7 +142,7 @@ public class HitBlox {
             w.getBlockAt(target_x, target_y, target_z).setTypeId(type);
         }
     }
- 
+
     /**
      * Returns STEPS forward along line of vision and returns block
      * 
@@ -170,11 +167,96 @@ public class HitBlox {
 
         } while ((length <= range) && ((target_x == last_x) && (target_y == last_y) && (target_z == last_z)));
 
-        if (length > range || target_y > 127 || target_y < 0) {
+        if (length > range || target_y > 255 || target_y < 0) {
             return null;
         }
 
         return w.getBlockAt(target_x, target_y, target_z);
+    }
+
+    public Block getRangeBlock() {
+        fromOffworld();
+        if (length > range) {
+            return null;
+        } else {
+            return getRange();
+        }
+    }
+
+    private Block getRange() {
+        last_x = target_x;
+        last_y = target_y;
+        last_z = target_z;
+
+        do {
+            length += step;
+
+            h_length = (length * rot_ycos);
+            y_offset = (length * rot_ysin);
+            x_offset = (h_length * rot_xcos);
+            z_offset = (h_length * rot_xsin);
+
+            target_x = (int) Math.floor(x_offset + player_x);
+            target_y = (int) Math.floor(y_offset + player_y);
+            target_z = (int) Math.floor(z_offset + player_z);
+
+        } while ((length <= range) && ((target_x == last_x) && (target_y == last_y) && (target_z == last_z)));
+
+        if (w.getBlockTypeIdAt(target_x, target_y, target_z) != 0) {
+            return w.getBlockAt(target_x, target_y, target_z);
+        }
+
+        if (length > range || target_y > 255 || target_y < 0) {
+            return w.getBlockAt(last_x, last_y, last_z);
+        } else {
+            return getRange();
+        }
+    }
+
+    public void fromOffworld() {
+        if (target_y > 255) {
+            while (target_y > 255 && length <= range) {
+                last_x = target_x;
+                last_y = target_y;
+                last_z = target_z;
+
+                do {
+                    length += step;
+
+                    h_length = (length * rot_ycos);
+                    y_offset = (length * rot_ysin);
+                    x_offset = (h_length * rot_xcos);
+                    z_offset = (h_length * rot_xsin);
+
+                    target_x = (int) Math.floor(x_offset + player_x);
+                    target_y = (int) Math.floor(y_offset + player_y);
+                    target_z = (int) Math.floor(z_offset + player_z);
+
+                } while ((length <= range) && ((target_x == last_x) && (target_y == last_y) && (target_z == last_z)));
+            }
+        } else if (target_y < 0) {
+            while (target_y < 0 && length <= range) {
+                last_x = target_x;
+                last_y = target_y;
+                last_z = target_z;
+
+                do {
+                    length += step;
+
+                    h_length = (length * rot_ycos);
+                    y_offset = (length * rot_ysin);
+                    x_offset = (h_length * rot_xcos);
+                    z_offset = (h_length * rot_xsin);
+
+                    target_x = (int) Math.floor(x_offset + player_x);
+                    target_y = (int) Math.floor(y_offset + player_y);
+                    target_z = (int) Math.floor(z_offset + player_z);
+
+                } while ((length <= range) && ((target_x == last_x) && (target_y == last_y) && (target_z == last_z)));
+            }
+        } else {
+            return;
+        }
     }
 
     /**
@@ -183,7 +265,7 @@ public class HitBlox {
      * @return Block
      */
     public Block getCurBlock() {
-        if (length > range || target_y > 127 || target_y < 0) {
+        if (length > range || target_y > 255 || target_y < 0) {
             return null;
         } else {
             return w.getBlockAt(target_x, target_y, target_z);
@@ -207,6 +289,9 @@ public class HitBlox {
      * @return Block
      */
     public Block getLastBlock() {
+        if (last_y > 255 || last_y < 0) {
+            return null;
+        }
         return w.getBlockAt(last_x, last_y, last_z);
     }
 
